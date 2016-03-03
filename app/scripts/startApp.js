a443faa0f70847cb1daf4dc473e67ff6bffd7179
riyvitxgt <zhukangmin@hotmail.com>
2016-03-03 18:01:51 +0800

    add some function

--- app/scripts/startApp.js
+++ app/scripts/startApp.js
@@ -7,6 +7,7 @@ angular.module('starterApp', ['ngMaterial'])
         $scope.addCols = [];
         $scope.updateOp = [];
         $scope.colums = [{
+            'id': 1,
             'isSelected': false,
             'columnName': 'id',
             'columnType': 'Int',
@@ -15,8 +16,9 @@ angular.module('starterApp', ['ngMaterial'])
             'notNull':true,
             'isPK':true,
             'defaultValue':0,
-            'description':'主键'
+            'remarks':'主键'
         },{
+            'id': 2,
             'isSelected': false,
             'columnName': 'name',
             'columnType': 'varchar',
@@ -25,8 +27,9 @@ angular.module('starterApp', ['ngMaterial'])
             'notNull':true,
             'isPK':false,
             'defaultValue':0,
-            'description':'名字'
+            'remarks':'名字'
         },{
+            'id': 3,
             'isSelected': false,
             'columnName': 'age',
             'columnType': 'Int',
@@ -35,11 +38,12 @@ angular.module('starterApp', ['ngMaterial'])
             'notNull':true,
             'isPK':false,
             'defaultValue':0,
-            'description':'年龄'
+            'remarks':'年龄'
         }];
         $scope.addColumn = function(){
             console.log($scope.colums.length);
             var newColumn = {
+                'id': new Date().getTime(),
                 isNewCol: true,
                 'columnName': '',
                 'columnType': '',
@@ -48,12 +52,17 @@ angular.module('starterApp', ['ngMaterial'])
                 'notNull':false,
                 'isPK':false,
                 'defaultValue':0,
-                'description':''
+                'remarks':''
             }
             $scope.colums.push(newColumn);
             console.log('dsdfs');
         };
         $scope.oldColumn = angular.copy($scope.colums);
+        $scope.save = function(){
+            var updates = save($scope.colums,  $scope.oldColumn);
+            updates.push.apply(updates,getDelUpdates($scope.deleteCols));
+            console.log("dsds");
+        }
         $scope.deleteColumn = function(){
             var indexes = [];
             for(var i = 0; i < $scope.colums.length; i++){
@@ -62,74 +71,200 @@ angular.module('starterApp', ['ngMaterial'])
                     $scope.deleteCols.push($scope.colums[i]);
                 }
             }
-            indexs.forEach(function(index){
-                $scope.colums.splice(index, 1);
-            });
+            for(var i = indexes.length - 1; i >= 0 ; i--){
+                $scope.colums.splice(indexes[i], 1);
+            }
         }
         $scope.saveUpdate = function(){
 
         };
-        $scope.$watch('colums',function(newValue,oldValue){
+        /*$scope.$watch('colums',function(newValue,oldValue){
             if(newValue.length != oldValue.length){
                 return;
             }
             var arr = getOperList(newValue, oldValue);
             $scope.updateOp.push.apply($scope.updateOp,arr);
-        },{deep:true});
+        },{deep:true});*/
     });
 
 
-function save(newValue, oldValue, delCols){
-    var operaArr = [];
+function getDelUpdates(delCols){
+    var updates = [];
+    delCols.forEach(function(col){
+        updates.push({
+            operaName: 'dropColumn',
+            columnName: col.columnName
+        });
+    });
+    return updates;
+}
+
+function save(newValue, oldValue){
+    var result = [];
     for(var i = 0; i < newValue.length; i++){
         if(newValue[i].isNewCol){
-            operaArr.push({
+            result.push.apply(result,addColumn(newValue[i]));
+            newValue[i].isNewCol = false;
+            /*result.push({
                 operaName: 'addColumn',
                 name: newValue[i].columnName,
                 nullable: newValue[i].notNull,
                 primaryKey: newValue[i].isPK
-            })
+            })*/
         }else{
-
-        }
-    }
-}
-
-function getOperList(newValue, oldValue){
-    var result = [];
-    for(var i = 0; i < oldValue.length; i++){
-        for(var key in oldValue[i]){
-            if(newValue[i][key] != oldValue[i][key]){
-                switch (key){
-                    case 'columnName':
-                        result.push({
-                            operaName:'renameColumn',
-                            oldColumnName:oldValue[i][key],
-                            newColumnName:newValue[i][key]
-                        });
-                        break;
-                    case 'columnType':
-                    case 'columnLength':
-                    case 'columnDecimal':
-                        result.push({
-                            operaName:'modifyDataType',
-                            columnName:newValue[i]['columnName'],
-                            columnLength: newValue[i]['columnLength'],
-                            columnDecimal: newValue[i]['columnDecimal'],
-                            newDataType:newValue[i]['columnName']
-                        });
-                        break;
-                    case 'notNull':
-                        result.push({
-                            operaName:newValue[i][key] ? 'dropNotNullConstraint' : 'addNotNullConstraint',
-                            columnName:newValue[i]['columnName'],
-                            columnLength: newValue[i]['columnLength'],
-                            columnDecimal: newValue[i]['columnDecimal'],
-                            newDataType:newValue[i]['columnName']
-                        });
+            for(var j = 0; j < oldValue.length; j++){
+                if(newValue[i].id == oldValue[j].id){
+                    result.push.apply(result,getOperList(newValue[i], oldValue[j]));
                 }
             }
         }
     }
     return result;
 }
+
+function getOperList(newValue, oldValue){
+    var result = [];
+    for(var key in oldValue){
+        if(newValue[key] != oldValue[key]){
+            switch (key){
+                case 'columnName':
+                    result.push(renameColumn(newValue,oldValue));
+                    break;
+                case 'columnType':
+                case 'columnLength':
+                case 'columnDecimal':
+                    result.push(modifyDataType(newValue));
+                    /*result.push({
+                        operaName:'modifyDataType',
+                        columnName:newValue['columnName'],
+                        columnLength: newValue['columnLength'],
+                        columnDecimal: newValue['columnDecimal'],
+                        newDataType:newValue['columnName']
+                    });*/
+                    break;
+                case 'notNull':
+                    result.push(nullConstraint(newValue))
+                   /* result.push({
+                        operaName:newValue[key] ? 'dropNotNullConstraint' : 'addNotNullConstraint',
+                        columnName:newValue['columnName'],
+                        columnLength: newValue['columnLength'],
+                        columnDecimal: newValue['columnDecimal'],
+                        newDataType:newValue['columnName']
+                    });*/
+            }
+        }
+    }
+    return result;
+}
+
+/**
+ * 添加字段
+ * @param newValue
+ * @returns {Array}
+ */
+function addColumn(newValue){
+    var update = [];
+    update.push({
+        operaName: 'addColumn',
+        name: newValue.columnName,
+        nullable: newValue.notNull,
+        primaryKey: newValue.isPK,
+        remarks: newValue.remarks
+    });
+    if(newValue.defaultValue){
+        update.push(addDefaultValue(newValue));
+    }
+    return update;
+}
+
+/**
+ * 添加默认值
+ * @param newValue
+ * @returns {{operaName: string, columnName: (string|string|string|string|*), defaultValue: (number|*|string)}}
+ */
+function addDefaultValue(newValue){
+    return {
+        operaName: 'addDefaultValue',
+        columnName: newValue.columnName,
+        defaultValue: newValue.defaultValue
+    }
+}
+/**
+ * 重命名字段名
+ * @param newValue
+ * @param oldValue
+ * @returns {{operaName: string, oldColumnName: (string|string|string|string|*), newColumnName: (string|string|string|string|*)}}
+ */
+function renameColumn(newValue, oldValue){
+    return {
+        operaName:'renameColumn',
+        oldColumnName:oldValue.columnName,
+        newColumnName:newValue.columnName,
+        remarks: newValue.remarks
+    };
+}
+
+/**
+ * 修改数据类型
+ * @param newValue
+ * @returns {{operaName: string, columnName: *, columnLength: *, columnDecimal: *, newDataType: *}}
+ */
+function modifyDataType(newValue){
+    return {
+        operaName:'modifyDataType',
+        columnName:newValue['columnName'],
+        columnLength: newValue['columnLength'],
+        columnDecimal: newValue['columnDecimal'],
+        newDataType:newValue['columnName']
+    }
+}
+
+/**
+ * 添加非空约束
+ * @param newValue
+ * @returns {{operaName: string, columnName: *, columnLength: *, columnDecimal: *, newDataType: *}}
+ */
+function nullConstraint(newValue){
+    return {
+        operaName:newValue['notNull'] ? 'dropNotNullConstraint' : 'addNotNullConstraint',
+        columnName:newValue['columnName'],
+        columnLength: newValue['columnLength'],
+        columnDecimal: newValue['columnDecimal'],
+        newDataType:newValue['columnName']
+    }
+}
+
+/*
+
+for(var key in oldValue[j]){
+    if(newValue[i][key] != oldValue[i][key]){
+        switch (key){
+            case 'columnName':
+                result.push({
+                    operaName:'renameColumn',
+                    oldColumnName:oldValue[i][key],
+                    newColumnName:newValue[i][key]
+                });
+                break;
+            case 'columnType':
+            case 'columnLength':
+            case 'columnDecimal':
+                result.push({
+                    operaName:'modifyDataType',
+                    columnName:newValue[i]['columnName'],
+                    columnLength: newValue[i]['columnLength'],
+                    columnDecimal: newValue[i]['columnDecimal'],
+                    newDataType:newValue[i]['columnName']
+                });
+                break;
+            case 'notNull':
+                result.push({
+                    operaName:newValue[i][key] ? 'dropNotNullConstraint' : 'addNotNullConstraint',
+                    columnName:newValue[i]['columnName'],
+                    columnLength: newValue[i]['columnLength'],
+                    columnDecimal: newValue[i]['columnDecimal'],
+                    newDataType:newValue[i]['columnName']
+                });
+        }
+    }
+}*/
